64902276f2b2965f447f47ff3f49ead4de7efe2f
brvnfr <ymmk93@gmail.com>
2023-11-14 23:05:34 -0300

    text-xs footer

--- src/components/UI/Footer.vue
+++ src/components/UI/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="w-full bg-zinc-100">
-    <div class="flex justify-center items-center text-center h-[25px]">
+    <div class="flex justify-center items-center text-center text-xs h-[25px]">
       <p>&copy; {{ currentYear }} {{ companyName }}</p>
     </div>
   </footer>
