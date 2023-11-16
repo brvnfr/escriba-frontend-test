72dcc8b828d67dd531e56c648b4243499b232d06
brvnfr <ymmk93@gmail.com>
2023-11-16 00:35:56 -0300

    [enchance] icon toggle sidebar

--- src/components/UI/Header.vue
+++ src/components/UI/Header.vue
@@ -2,7 +2,7 @@
   <header class="w-full bg-zinc-900">
     <div class="flex justify-between items-center h-[50px]">
       <div @click="clickHamburger" :class="['p-3 cursor-pointer border-rounded text-white', 'hover:bg-zinc-800']">
-        <font-awesome-icon :icon="['fas', 'bars']" />
+        <font-awesome-icon :icon="['fas', dataOpenSideBar ? 'bars-staggered' : 'bars']" />
       </div>
 
       <div class="p-3 h-[50px] w-[50px] bg-yellow-400 text-center hover:bg-yellow-500">
